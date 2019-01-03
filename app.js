f5508409ce3a114a193c2f5969227ec6706fe9e6
gomepapa <papa4children@outlook.jp>
2019-01-03 17:22:14 +0900

    フォーム送信処理を追加

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
 const url = require('url');
+const qs = require('querystring');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
 const other_page = fs.readFileSync('./other.ejs', 'utf8');
@@ -16,39 +17,15 @@ console.log('Server start!');
 
 // createServerの処理
 function getFromClient(request, response) {
-    let url_parts = url.parse(request.url);
-    let content;
+    let url_parts = url.parse(request.url, true);
 
     switch (url_parts.pathname) {
         case '/':
-            content = ejs.render(
-                index_page
-                , {
-                    title: 'Index'
-                    , content: 'これはテンプレートを使ったサンプルページです。'
-                }
-            );
-        
-            response.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            response.write(content);
-            response.end();
+            response_index(request, response, url_parts);
             break;
         
         case '/other':
-            content = ejs.render(
-                other_page
-                , {
-                    title: 'other'
-                    , content: 'これは新しく用意したページです。'
-                }
-            );
-            response.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            response.write(content);
-            response.end();
+            response_other(request, response);
             break;
         
         case '/style.css':
@@ -65,5 +42,82 @@ function getFromClient(request, response) {
             });
             response.end('no page...');
             break;
+    }
+}
+
+// indexのアクセス処理
+function response_index(request, response, url_parts) {
+    let query = url_parts.query;
+            
+    let content_body = 'これはIndexページです。';
+
+    if (query.msg !== undefined) {
+        content_body += 'あなたは「' + query.msg + '」と送りました。';
+    }
+    
+    let content = ejs.render(
+        index_page
+        , {
+            title: 'Index'
+            , content: content_body
+        }
+    );
+
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    response.write(content);
+    response.end();
+}
+
+// otherのアクセス処理
+function response_other(request, response) {
+    let content;
+    let content_body = 'これはOtherページです。';
+    
+    if (request.method === 'POST') { // POSTアクセス時の処理
+        let body = '';
+        
+        // データ受信時のイベント処理
+        request.on('data', (data) => {
+            body += data;
+        });
+        
+        // データ受信終了のイベント処理
+        request.on('end', () => {
+            let post_data = qs.parse(body);
+            content_body += 'あなたは「' + post_data.msg + '」と書きました。';
+
+            content = ejs.render(
+                other_page
+                , {
+                    title: 'other'
+                    , content: content_body
+                }
+            );
+        
+            response.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            response.write(content);
+            response.end();
+        });
+
+    } else { // GETアクセス時の処理
+        content_body = 'POST送信されたデータはありません。';
+
+        content = ejs.render(
+            other_page
+            , {
+                title: 'other'
+                , content: content_body
+            }
+        );
+    
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        response.write(content);
+        response.end();
     }
 }
